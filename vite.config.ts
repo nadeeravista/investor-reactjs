fddf5b3201ac12850e0387e8edae5d8413317ef4
Nadeera <nadeera.vista@gmail.com>
2024-11-27 02:05:57 +1300

    Adding tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,38 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import eslint from 'vite-plugin-eslint';
+import { URL, fileURLToPath } from 'url';
+
+const eslintParams = {
+  exclude: ['**/*.svg'],
+  include: ['./src/**/*'],
+};
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      // default settings on build (i.e. fail on error)
+      ...eslint(eslintParams),
+      apply: 'build',
+    },
+    {
+      ...eslint({
+        ...eslintParams,
+        failOnWarning: false,
+        failOnError: false,
+      }),
+      apply: 'serve',
+      enforce: 'post',
+    },
+  ],
+  resolve: {
+    alias: {
+      '@components': fileURLToPath(
+        new URL('./src/components', import.meta.url)
+      ),
+      '@types': fileURLToPath(new URL('./src/types', import.meta.url)),
+    },
+  },
 });
